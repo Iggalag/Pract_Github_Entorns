7ac2b6ca827018f4a35424eeab3f13eb0259f5ac
Andreas De La Iglesia Ars <andreas.delaiglesia@epia.granollers>
2018-02-08 18:54:53 +0100

    Catura de teclas terminada. close #4

--- Programa/Source.cpp
+++ Programa/Source.cpp
@@ -10,10 +10,31 @@ int main(int argc, char* args[]){
 	SDL_Renderer* gRenderer = nullptr;
 	gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 	gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	SDL_Event test_event;
 
 	do
 	{
 
+		while (SDL_PollEvent(&test_event)) {
+			switch (test_event.type) {
+			case SDL_KEYDOWN:
+				switch (test_event.key.keysym.scancode) {
+				case SDL_SCANCODE_ESCAPE:
+					break;
+				case SDL_SCANCODE_1:
+					break;
+				case SDL_SCANCODE_2:
+					break;
+				default:
+					break;
+				}
+				break;
+
+			case SDL_QUIT:
+				fin = true;
+				break;
+			}
+		}
 	} while (fin == false);
 
 	return 0;
